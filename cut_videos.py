2a60512405dccc05e237dbd957dbda9d0e243abd
Pedro Dal Bianco <dalbianco.pedro@gmail.com>
2021-07-08 16:48:22 -0300

    Updated DB

--- cut_videos.py
+++ cut_videos.py
@@ -1,14 +1,23 @@
 import json
 import os
-from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+from moviepy.video.io.VideoFileClip import VideoFileClip
+# La opción usando esta función es (mucho) más rápida pero deja framse congelados al final
+#from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 
 
 with open("data/subs_dict.json", 'r', encoding='utf-8') as subs_dict_file:
   subs_dict: dict[str, list[tuple[float, float, str]]] = json.load(subs_dict_file)
 
-for name, subs in subs_dict.items():
+# Se descarta un video con los subtitulos descoordinados
+items = [(n,s) for (n,s) in subs_dict.items() if n != 'Noticias en Lengua de Señas Argentina (resumen semanal 06_12_2020)-d7akwvWNPrU']
+
+for name, subs in items:
     dir = "data/cuts/{}/".format(name)
     if not os.path.isdir(dir):
         os.mkdir(dir)
-    for i, (start, end, sub) in enumerate(subs):
-        ffmpeg_extract_subclip("raw/{}.mp4".format(name), start, end, targetname=(dir + str(i) + ".mp4"))
+    with VideoFileClip("raw/{}.mp4".format(name)) as video:
+        for i, (start, end, sub) in enumerate(subs):
+            if not os.path.isfile((dir + str(i) + ".mp4")):
+                new = video.subclip(start, end)
+                new.write_videofile((dir + str(i) + ".mp4"), audio=False)
+        #ffmpeg_extract_subclip("raw/{}.mp4".format(name), start, end, targetname=(dir + str(i) + ".mp4"))
