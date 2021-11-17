676cd616bf28a728cb0149525375db92dd1b88d3
Pedro Dal Bianco <dalbianco.pedro@gmail.com>
2021-11-17 10:37:19 -0300

    Update download.py

--- download.py
+++ download.py
@@ -9,18 +9,13 @@ playlists = ["https://www.youtube.com/playlist?list=PLhysX0rYXWV2xM3T4KAqaAEg-GE
             "https://www.youtube.com/playlist?list=PLhysX0rYXWV2wQnK2nxU4gvcmq5yrHli3",
             "https://www.youtube.com/playlist?list=PLhysX0rYXWV2WNLgIyBiyn3wizILOTuP6"]
 
-videos: list[YouTube] = [item for sublist in map(lambda x: Playlist(x).videos, playlists) for item in sublist]
+videos: list[YouTube] = [item for sublist in map(lambda x: Playlist(x).videos, playlists) for item in sublist if 'es-419' in item.captions]
 
-t = 0
 for idx, yt in enumerate(videos):
     yt.register_on_progress_callback(on_progress)
     st = yt.streams.filter(adaptive=True, file_extension='mp4').order_by('resolution').last()
     print("\nVideo {}/{}".format(idx + 1, len(videos)), '\n', st)
-    #if not os.path.isfile('./raw/' + yt.title.replace('/', '-') + '.vtt') and st is not None and 'es-419' in yt.captions:
-    if st is not None and 'es-419' in yt.captions:
-        #with open('./raw/' + yt.title.replace('/', '-') + '.vtt', 'w') as subs_file:
-        #    subs_file.write(yt.captions['es-419'].generate_srt_captions())
-        #st.download('./raw/')
-        t = t + 1
-print('{} videos y subtítulos descargados'.format(t))
-        
+    if not os.path.isfile('./raw/' + yt.title.replace('/', '-') + '.vtt') and st is not None:
+        with open('./raw/' + yt.title.replace('/', '-') + '.vtt', 'w') as subs_file:
+            subs_file.write(yt.captions['es-419'].generate_srt_captions())
+        st.download('./raw/')
