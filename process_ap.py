3615a06ec32453e094a3807423cc8ca77db333b9
Pedro Dal Bianco <dalbianco.pedro@gmail.com>
2021-11-29 21:54:57 -0300

    Arreglado error detectando los senantes

--- process_ap.py
+++ process_ap.py
@@ -1,10 +1,9 @@
 import json
 import os
-from typing import TypedDict, TypeVar
+from typing import TypedDict
 from statistics import mean
 import numpy as np
 
-T = TypeVar("T")
 
 class Box(TypedDict):
     x1: float
@@ -24,10 +23,13 @@ def group_kds(kds: list[KeypointData]) -> list[list[KeypointData]]:
     '''Groups keypoint data objects that belong to same frame'''
     grouped: list[list[KeypointData]] = [[]]
     for kd in kds:
+        added = False
         for g in grouped:
-            if not g or g[-1]['image_id'] != kd['image_id']:
+            if not added and (not g or g[-1]['image_id'] != kd['image_id']):
                 g.append(kd)
-                break
+                added = True
+        if not added:
+            grouped.append([kd])
     return grouped
 
 def get_box(signer: list[KeypointData]) -> Box:
@@ -37,30 +39,30 @@ def get_box(signer: list[KeypointData]) -> Box:
         'width': signer[0]['box'][2],
         'height': signer[0]['box'][3]
     }
-
     for keydata in signer:
         box['x1'] = min(box['x1'], keydata['box'][0])
         box['y1'] = min(box['y1'], keydata['box'][1])
         box['width'] = max(box['width'], keydata['box'][2])
         box['height'] = max(box['height'], keydata['box'][3])
-
     return box
 
+has_ap = lambda c: os.path.isfile("data/test/" + c + "/alphapose-results.json")
+
+#cuts = [d for cut in os.listdir("data/cuts") for d in os.listdir("data/cuts/" + cut) if has_ap(d)]
+tests = [d for d in os.listdir("data/test") if has_ap(d)]
 # Identify signers
-#for cut in os.listdir("data/cuts"):
-#    for f in os.listdir("data/cuts/" + cut):
-for f in [d for d in os.listdir("data/test") if os.path.isfile("data/test/" + d + "/alphapose-results.json")]:
+for idx, f in enumerate(tests):
     #path = "data/cuts/" + cut + '/' + f
     path = "data/test/" + f
-    print(path)
+    print("{}/{}".format(idx, len(tests)))
 
     with open(path + "/alphapose-results.json") as ap_file:
-        # seqs contains a list of lists of keypoints data, one for each signer
-        seqs: list[list[KeypointData]] = group_kds(json.load(ap_file))
+        # signers contains a list of lists of keypoints data, one for each signer
+        signers: list[list[KeypointData]] = group_kds(json.load(ap_file))
 
-    keypoints: list[dict[str,list[list]]] = []
-    for s in seqs:
-        keypoints.append({
+    keypoints_for_signers: list[dict[str,list[list]]] = []
+    for s in signers:
+        keypoints_for_signers.append({
             'x': [[] for _ in range(136)],
             'y': [[] for _ in range(136)],
             'c': [[] for _ in range(136)]
@@ -68,27 +70,24 @@ for f in [d for d in os.listdir("data/test") if os.path.isfile("data/test/" + d 
         for keydata in s:
             for idx, keypoint in enumerate(keydata["keypoints"]):
                 if idx % 3 == 0:
-                    keypoints[-1]['x'][int(idx/3)].append(keypoint)
+                    keypoints_for_signers[-1]['x'][int(idx/3)].append(keypoint)
                 if idx % 3 == 1:
-                    keypoints[-1]['y'][int(idx/3)].append(keypoint)
+                    keypoints_for_signers[-1]['y'][int(idx/3)].append(keypoint)
                 if idx % 3 == 2:
-                    keypoints[-1]['c'][int(idx/3)].append(keypoint)
-
-    # print(len(seqs), len(seqs[1]), len(seqs[0][0]["keypoints"]))
-    # print(len(keypoints), len(keypoints[0]['x']), len(keypoints[0]['x'][0]))
+                    keypoints_for_signers[-1]['c'][int(idx/3)].append(keypoint)
 
     max_idx, max_total = 0, None
-    for idx, each in enumerate(keypoints):
+    for idx, each in enumerate(keypoints_for_signers):
         distance_x = np.array(list(map(lambda keys: max(keys) - min(keys), each['x'])))
         distance_y = np.array(list(map(lambda keys: max(keys) - min(keys), each['y'])))
         distance = np.sqrt(distance_x**2 + distance_y**2)
-        confidence = np.array(list(map(mean, each['c'])))
-        total = np.sum(np.multiply(distance, confidence))
+        #confidence = np.array(list(map(max, each['c'])))
+        total = np.sum(distance[:94])
         if max_total is None or max_total < total:
             max_idx = idx
             max_total = total
 
-    signer = seqs[max_idx]
+    signer = signers[max_idx]
 
     with open(path + ".json", "r+", encoding="utf-8") as res_file:
         res = json.load(res_file)
