934aff663c3a50f150b79d838cf809781e8c533d
pedroodb <dalbianco.pedro@gmail.com>
2021-12-02 14:03:59 -0300

    Updated count_ap.sh

--- count_ap.sh
+++ count_ap.sh
@@ -2,16 +2,16 @@
 
 IFS=$'\n'; set -f;
 total=0
-missing=0
+completed=0
 
 for vid in $(find 'data/cuts' -name '*.mp4'); do
-	if [ ! -f "${vid::-4}/alphapose-results.json" ]; then
-		missing=$((missing+1))
+	if [ -f "${vid::-4}/alphapose-results.json" ]; then
+		completed=$((completed+1))
 	fi;
 	total=$((total+1))
 done;
 
-echo $total
-echo $missing
+echo "$completed / $total"
 
 unset IFS; set +f;
+
