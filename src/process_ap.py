7f1b583fcfaea677ea86681e97378023a9e8f7e3
pedroodb <dalbianco.pedro@gmail.com>
2022-02-08 15:30:45 -0300

    La suma total de distancia la normalizaría dividiendo con la cantidad de frames, ahora quizás es lo mismo porque solo comparás entre signers de un mismo video que dura lo mismo para los dos, pero si querés comparar entre videos de distinta longitud no se podría. Además es mejor pa debuggear, porque si la distancia normalizada te da 4, sabés que se movió 4 pixels en cada frame; si te da 40, sabés que se movió 40 en todo el video pero no sabés si el mismo duró 10 frames o 100.

--- src/process_ap.py
+++ src/process_ap.py
@@ -19,6 +19,7 @@ class KeypointData(TypedDict):
     idx: list[float]
 
 def get_cut_paths(cut: Path) -> dict[str, Path]:
+    'Return paths for al the files (if exists) corresponding to a single clip'
     name = cut.name[:-4]
     return {
         'mp4': cut,
@@ -29,6 +30,7 @@ def get_cut_paths(cut: Path) -> dict[str, Path]:
     }
 
 def format_box(box: list[float]) -> Box:
+    'Creates box from list of strings'
     return {
         'x1': box[0],
         'y1': box[1],
@@ -37,7 +39,7 @@ def format_box(box: list[float]) -> Box:
     }
 
 def group_kds(kds: list[KeypointData]) -> list[list[KeypointData]]:
-    '''Groups keypoint data objects that belong to same frame'''
+    'Groups keypoint data objects that belong to same frame'
     grouped: list[list[KeypointData]] = [[]]
     for kd in kds:
         added = False
@@ -50,6 +52,7 @@ def group_kds(kds: list[KeypointData]) -> list[list[KeypointData]]:
     return grouped
 
 def get_box(signer: list[KeypointData]) -> Box:
+    'Get box of signer alongside the entire clip'
     box: Box = {
         'x1': signer[0]['box'][0],
         'y1': signer[0]['box'][1],
@@ -64,6 +67,7 @@ def get_box(signer: list[KeypointData]) -> Box:
     return box
 
 def relative_pos(box: Box, x: float, y: float) -> tuple[float, float]:
+    'Returns relative position of a keypoint respect to the signers box center'
     center_x = box['x1'] + box['width']/2
     center_y = box['y1'] + box['height']/2
     return (center_x - x, center_y - y)
@@ -110,16 +114,19 @@ def main():
         for i_signer, each in enumerate(keypoints_for_signers):
             distance = 0
             for i_keyp in range(94, len(each['c'])):
+                # for each keypoint there is its x and y position and confidence
                 xs = each['x'][i_keyp]
                 ys = each['y'][i_keyp]
                 cs = each['c'][i_keyp]
                 for i_frame in range(0, len(cs), step):
+                    # we only consider keypoints with confidence greater than 0.5
                     if i_frame + step < len(cs) and cs[i_frame] > 0.5 and cs[i_frame + step] > 0.5:
                         box1, box2 = signers[i_signer][i_frame]['box'], signers[i_signer][i_frame + step]['box']
                         rel_x1, rel_y1 = relative_pos(format_box(box1), xs[i_frame], ys[i_frame])
                         rel_x2, rel_y2 = relative_pos(format_box(box2), xs[i_frame + step], ys[i_frame + step])
                         distance += sqrt((rel_x1 - rel_x2)**2 + (rel_y1 - rel_y2)**2)
-            scores.append(distance)
+            # movement is normalized respect to the amount of frames
+            scores.append(distance / len(cs))
 
         signer = signers[scores.index(max(scores))]
 
