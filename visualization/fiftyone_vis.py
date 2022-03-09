1f392b5a6986418746531dbe3bf1b64196050690
pedroodb <dalbianco.pedro@gmail.com>
2022-03-09 15:46:40 -0300

    Visualizacion con 51

--- visualization/fiftyone_vis.py
+++ visualization/fiftyone_vis.py
@@ -1,8 +1,7 @@
 import fiftyone as fo
-import json
+import argparse, json
 from pathlib import Path
 from fiftyone import Sample
-from multiprocessing import Pool
 
 
 def get_score(scores):
@@ -11,7 +10,7 @@ def get_score(scores):
     scores.remove(m1)
     return 0 if m1 == 0 else (m1 - max(scores)) / m1
 
-def store_sample(clip_file, dataset):
+def store_sample(clip_file: Path, dataset):
     print(clip_file)
     sample = Sample(clip_file)
     with Path(str(clip_file.resolve())[:-3] + "json").open() as sample_data_file:
@@ -41,19 +40,26 @@ def store_sample(clip_file, dataset):
             for i in range(93, len(keydata["keypoints"]), 3) if keydata["keypoints"][i+2] > 0.5])
     dataset.add_sample(sample)
 
-#fo.load_dataset("cn_sordos").delete()
-dataset = fo.Dataset("cn_sordos")
-path = Path("./cuts")
-clips = list(path.rglob("*.mp4"))[:10]
+def main():
+    parser = argparse.ArgumentParser(description='''Generates DB able to visualize clips on fiftyone and starts fiftyone.''')
+    parser.add_argument('--generate', '-g', help='generates the database, only necessary the first time running the script', action='store_true')
+    must_gen: bool = parser.parse_args().generate
 
-'''pool = Pool()
-pool.map(store_sample, clips)
-pool.close()
-pool.join()'''
-for c in clips:
-    store_sample(c, dataset)
+    if must_gen:
+        fo.load_dataset("cn_sordos").delete()
+        dataset = fo.Dataset("cn_sordos", persistent=True)
+        path = Path("./data/cuts")
+        clips = list(path.rglob("*.mp4"))
+        for i,c in enumerate(clips,1):
+            print(f"{i}/{len(clips)}")
+            store_sample(c, dataset)
+    else:
+        dataset = fo.load_dataset("cn_sordos")
 
-# View summary info about the dataset
-print(dataset)
-sess = fo.launch_app(dataset=dataset)
-sess.wait()
+    # View summary info about the dataset
+    print(dataset)
+    sess = fo.launch_app(dataset=dataset)
+    sess.wait()
+
+if __name__ == "__main__":
+    main()
